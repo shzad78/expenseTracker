487202188664f6b011452672d477b28cedcceff9
shzad78 <shzad78@gmail.com>
2021-09-09 15:00:57 +0200

    expenxe

--- src/transContext.js
+++ src/transContext.js
@@ -18,7 +18,7 @@ export const TransactionProvider = ({})=>{
                     amount: transOnj.amount,
                     desc: transObj.desc
 
-                }
+                } 
             })
     }
 }
